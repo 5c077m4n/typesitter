cdfed805171cff1dbb8332a42292ce2848296415
Roee Shapira <ro33.sha@gmail.com>
2022-01-06 22:33:11 +0200

    Add repl mode

--- src/lib/ast_walker.rs
+++ src/lib/ast_walker.rs
@@ -1,6 +1,17 @@
+use anyhow::Result;
 use log::*;
 use tree_sitter::TreeCursor;
 
-pub fn walk(cursor: &TreeCursor) {
-    debug!("{:?}", &cursor.node().to_sexp());
+pub fn walk(cursor: &mut TreeCursor) -> Result<()> {
+    let node = cursor.node();
+    debug!("{:#?}", &node);
+
+    while cursor.goto_first_child() {
+        walk(cursor)?;
+    }
+	while cursor.goto_next_sibling() {
+        walk(cursor)?;
+	}
+
+	Ok(())
 }
