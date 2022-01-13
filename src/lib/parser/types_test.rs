259498cfbff7569a125400ac962a943f9a1f12f2
Roee Shapira <ro33.sha@gmail.com>
2022-01-13 13:38:18 +0200

    Remove dead code && added tests

--- src/lib/parser/types_test.rs
+++ src/lib/parser/types_test.rs
@@ -1,6 +1,21 @@
 use super::types::*;
 
 #[test]
-fn number_test() {
+fn float_short_test() {
 	assert_eq!(".42".parse::<Number>(), Ok(Number(0.42)));
+}
+
+#[test]
+fn float_long_test() {
+	assert_eq!("1.42".parse::<Number>(), Ok(Number(1.42)));
+}
+
+#[test]
+fn float_as_int_test() {
+	assert_eq!("42.".parse::<Number>(), Ok(Number(42.)));
+}
+
+#[test]
+fn decimal_test() {
+	assert_eq!("42".parse::<Number>(), Ok(Number(42.)));
 }
