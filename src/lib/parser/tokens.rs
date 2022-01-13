abdc34b7d22e8143164f9ef5813d11276b1daa32
Roee Shapira <ro33.sha@gmail.com>
2022-01-13 12:38:07 +0200

    Add Number type and parser

--- src/lib/parser/tokens.rs
+++ src/lib/parser/tokens.rs
@@ -2,20 +2,20 @@
 
 use nom::{
 	branch::alt,
-	bytes::complete::{is_not, tag, take_until},
-	character::complete::{char, multispace0, one_of},
-	combinator::{opt, recognize, value},
+	bytes::complete::{tag, take_until},
+	character::complete::{char, digit1, multispace0, not_line_ending, one_of},
+	combinator::{recognize, value},
 	error::ParseError,
 	multi::{many0, many1},
 	sequence::{delimited, pair, preceded, terminated, tuple},
 	IResult,
 };
 
-const EOL: &str = if cfg!(windows) { "\r\n" } else { "\n" };
-
 /// A combinator that takes a parser `inner` and produces a parser that also consumes both leading and
 /// trailing whitespace, returning the output of `inner`.
-pub fn rm_surround_ws<'a, F: 'a, O, E>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+pub fn rm_surround_ws<'a, F: 'a, O, E: 'a>(
+	inner: F,
+) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
 where
 	F: Fn(&'a str) -> IResult<&'a str, O, E>,
 	E: ParseError<&'a str>,
@@ -29,7 +29,7 @@ where
 {
 	value(
 		(), // Output is thrown away.
-		pair(tag("//"), is_not("\n\r")),
+		pair(tag("//"), not_line_ending),
 	)(input)
 }
 
@@ -50,27 +50,6 @@ pub fn binary(input: &str) -> IResult<&str, &str> {
 	)(input)
 }
 
-pub fn float(input: &str) -> IResult<&str, &str> {
-	alt((
-		// Case one: .42
-		recognize(tuple((
-			char('.'),
-			decimal,
-			opt(tuple((one_of("eE"), opt(one_of("+-")), decimal))),
-		))),
-		// Case two: 42e42 and 42.42e42
-		recognize(tuple((
-			decimal,
-			opt(preceded(char('.'), decimal)),
-			one_of("eE"),
-			opt(one_of("+-")),
-			decimal,
-		))),
-		// Case three: 42. and 42.42
-		recognize(tuple((decimal, char('.'), opt(decimal)))),
-	))(input)
-}
-
 pub fn decimal(input: &str) -> IResult<&str, &str> {
-	recognize(many1(terminated(one_of("0123456789"), many0(char('_')))))(input)
+	recognize(many1(terminated(digit1, many0(char('_')))))(input)
 }
