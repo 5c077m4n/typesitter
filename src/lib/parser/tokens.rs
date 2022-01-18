db175d76bbc8e6b44863865e558878c1deaceebd
Roee Shapira <ro33.sha@gmail.com>
2022-01-19 00:26:17 +0200

    Add wasm op codes and leb128

--- src/lib/parser/tokens.rs
+++ src/lib/parser/tokens.rs
@@ -3,25 +3,12 @@
 use nom::{
 	branch::alt,
 	bytes::complete::{tag, take_till},
-	character::complete::{alphanumeric1, char, digit1, multispace0, one_of},
+	character::complete::{alphanumeric1, char, digit1, multispace0, multispace1, one_of},
 	combinator::recognize,
-	error::ParseError,
 	multi::{many0, many1},
 	sequence::{delimited, preceded, terminated, tuple},
 	IResult,
 };
-
-/// A combinator that takes a parser `inner` and produces a parser that also consumes both leading and
-/// trailing whitespace, returning the output of `inner`.
-pub fn rm_surround_ws<'a, F: 'a, O, E: 'a>(
-	inner: F,
-) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
-where
-	F: Fn(&'a str) -> IResult<&'a str, O, E>,
-	E: ParseError<&'a str>,
-{
-	delimited(multispace0, inner, multispace0)
-}
 
 pub fn binary(input: &str) -> IResult<&str, &str> {
 	preceded(
@@ -41,14 +28,15 @@ pub fn string(input: &str) -> IResult<&str, &str> {
 	))(input)
 }
 
-pub fn var_init(input: &str) -> IResult<&str, (&str, &str)> {
-	let (tail, (_, var_name, _, expr, _)) = tuple((
-		alt((tag("const "), tag("let "))),
+pub fn var_init(input: &str) -> IResult<&str, (&str, &str, &str)> {
+	let (tail, (var_type, _, var_name, _, expr, _)) = tuple((
+		alt((tag("const"), tag("let"))),
+		multispace1,
 		alphanumeric1,
-		rm_surround_ws(char('=')),
+		delimited(multispace0, char('='), multispace0),
 		take_till(|c| c == ';'),
-        char(';'),
+		char(';'),
 	))(input)?;
 
-	Ok((tail, (var_name, expr)))
+	Ok((tail, (var_type, var_name, expr)))
 }
