e367b361dfbfdd53a193c15c6012f4da59c49b89
Roee Shapira <roee.shapira@tensorleap.ai>
2022-01-18 23:28:07 +0200

    Add parsers

--- src/lib/parser/mod.rs
+++ src/lib/parser/mod.rs
@@ -1,7 +1,4 @@
 pub mod tokens;
-pub mod types;
 
 #[cfg(test)]
 mod tokens_test;
-#[cfg(test)]
-mod types_test;
--- src/lib/parser/tokens.rs
+++ src/lib/parser/tokens.rs
@@ -2,12 +2,12 @@
 
 use nom::{
 	branch::alt,
-	bytes::complete::tag,
-	character::complete::{char, digit1, multispace0, one_of},
+	bytes::complete::{tag, take_till},
+	character::complete::{alphanumeric1, char, digit1, multispace0, one_of},
 	combinator::recognize,
 	error::ParseError,
 	multi::{many0, many1},
-	sequence::{delimited, preceded, terminated},
+	sequence::{delimited, preceded, terminated, tuple},
 	IResult,
 };
 
@@ -25,11 +25,30 @@ where
 
 pub fn binary(input: &str) -> IResult<&str, &str> {
 	preceded(
-		alt((tag("0b"), tag("0B"))),
+		tag("0b"),
 		recognize(many1(terminated(one_of("01"), many0(char('_'))))),
 	)(input)
 }
 
 pub fn decimal(input: &str) -> IResult<&str, &str> {
 	recognize(many1(terminated(digit1, many0(char('_')))))(input)
+}
+
+pub fn string(input: &str) -> IResult<&str, &str> {
+	alt((
+		delimited(char('\''), take_till(|c| c == '\''), char('\'')),
+		delimited(char('"'), take_till(|c| c == '"'), char('"')),
+	))(input)
+}
+
+pub fn var_init(input: &str) -> IResult<&str, (&str, &str)> {
+	let (tail, (_, var_name, _, expr, _)) = tuple((
+		alt((tag("const "), tag("let "))),
+		alphanumeric1,
+		rm_surround_ws(char('=')),
+		take_till(|c| c == ';'),
+        char(';'),
+	))(input)?;
+
+	Ok((tail, (var_name, expr)))
 }
