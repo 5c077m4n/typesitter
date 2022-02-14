8d8f21e921cdd6685d8b385591dd6cbcc197b598
Roee Shapira <ro33.sha@gmail.com>
2022-02-14 11:15:57 +0200

    Add integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -4,19 +4,41 @@ use assert_cmd::Command;
 const BIN_NAME: &str = env!("CARGO_PKG_NAME");
 
 #[test]
-#[ignore]
-fn file_read_test() -> Result<()> {
+fn eval_number_var_init_test() -> Result<()> {
 	let mut cmd = Command::cargo_bin(BIN_NAME)?;
 
-	cmd.arg("tests/assets/1.ts").assert().success();
+	cmd.args(&["--eval", "const n: number = 123;"])
+		.assert()
+		.success();
+	Ok(())
+}
+
+#[test]
+fn eval_string_var_init_test() -> Result<()> {
+	let mut cmd = Command::cargo_bin(BIN_NAME)?;
+
+	cmd.args(&["--eval", r#"const s: string = 'qwerty';"#])
+		.assert()
+		.success();
+	Ok(())
+}
+
+#[test]
+fn stdin_test() -> Result<()> {
+	let mut cmd = Command::cargo_bin(BIN_NAME)?;
+
+	cmd.write_stdin("const n: number = 1234;")
+		.write_stdin("\n")
+		.assert()
+		.success();
 	Ok(())
 }
 
 #[test]
 #[ignore]
-fn eval_command_test() -> Result<()> {
+fn file_read_test() -> Result<()> {
 	let mut cmd = Command::cargo_bin(BIN_NAME)?;
 
-	cmd.arg("--eval='console.log(123);'").assert().success();
+	cmd.arg("tests/assets/1.ts").assert().success();
 	Ok(())
 }
