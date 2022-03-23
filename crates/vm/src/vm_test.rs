4b61a3a39acbada07c60126769c7a447f2b2df7f
Roee Shapira <ro33.sha@gmail.com>
2022-03-24 00:23:51 +0200

    Refactor interpret fn

--- crates/vm/src/vm_test.rs
+++ crates/vm/src/vm_test.rs
@@ -1,8 +1,7 @@
 use super::{instr::Instr, vm::VM};
-use anyhow::Result;
 
 #[test]
-fn sanity() -> Result<()> {
+fn simple_calc() {
 	let mut vm = VM::default();
 	let result = vm.interpret(&[
 		Instr::Push(9.),
@@ -13,5 +12,46 @@ fn sanity() -> Result<()> {
 	]);
 
 	assert_eq!(result, Some(5.));
-	Ok(())
+}
+
+#[test]
+#[ignore]
+fn sum_first_100_ints() {
+	let mut vm = VM::default();
+	let result = vm.interpret(&[
+		// setup
+		Instr::Push(0.), // the accumulator
+		Instr::Push(0.), // the index
+		// loop
+		// First, add the index to the accumulator
+		// stack: [accumulator, index]
+		Instr::Get(0),
+		Instr::Get(1),
+		// stack: [accumulator, index, accumulator, index]
+		Instr::Add,
+		// stack: [accumulator, index, accumulator + index]
+		Instr::Set(0),
+		Instr::Pop,
+		// stack: [accumulator + index, index]
+
+		// next, increment the index
+		Instr::Push(1.), // the increment
+		// stack: [accumulator, index, 1]
+		Instr::Add,
+		// stack: [accumulator, index + 1]
+
+		// finally, compare the index with 100 and jump back to the start
+		// if they're not equal.
+		Instr::Get(1),
+		// stack: [accumulator, index, index]
+		Instr::Push(100.),
+		Instr::Sub,
+		// stack: [accumulator, index, index - 100]
+		Instr::JumpNotEqual(2),
+		// if index - 100 == 0, print the accumulator
+		Instr::Get(0),
+		// stack: [accumulator, index, 0, accumulator]
+	]);
+
+	assert_eq!(result, Some(100.));
 }
