688ad4801f677cf725089b31d4c74c7f1b639d4b
Roee Shapira <ro33.sha@gmail.com>
2022-03-24 10:45:37 +0200

    Fix sum first 100 test

--- crates/vm/src/vm_test.rs
+++ crates/vm/src/vm_test.rs
@@ -1,23 +1,21 @@
 use super::{instr::Instr, vm::VM};
 
 #[test]
-fn simple_calc_one_plus_one() {
+fn simple_arith_one_plus_one() {
+	use Instr::*;
+
 	let mut vm = VM::default();
-	let result = vm.interpret(&[Instr::Push(1.), Instr::Push(1.), Instr::Add]);
+	let result = vm.interpret(&[Push(1.), Push(1.), Instr::Add]);
 
 	assert_eq!(result, Some(2.));
 }
 
 #[test]
 fn complex_arith() {
+	use Instr::*;
+
 	let mut vm = VM::default();
-	let result = vm.interpret(&[
-		Instr::Push(9.),
-		Instr::Push(3.),
-		Instr::Push(1.),
-		Instr::Add,
-		Instr::Sub,
-	]);
+	let result = vm.interpret(&[Push(9.), Push(3.), Push(1.), Add, Sub]);
 
 	assert_eq!(result, Some(5.));
 }
@@ -25,10 +23,26 @@ fn complex_arith() {
 #[test]
 #[ignore]
 fn sum_first_100_ints() {
+	use Instr::*;
+
 	let mut vm = VM::default();
 	let result = vm.interpret(&[
-		Instr::Push(0.), // [accumilator = 0]
-		Instr::Push(0.), // [accumilator = 0, index = 0]
+		Push(0.), // [accumilator = 0]
+		Push(1.), // [accumilator = 0, index = 1]
+		// stack: [accumilator, index]
+		Get(0), // [accumilator, index, accumilator] <- Loop start - jump to here
+		Get(1), // [accumilator, index, accumilator, index]
+		Add,    // [accumilator, index, (accumilator + index)]
+		Set(0), // [accumilator + (accumilator + index), index, (accumilator + index)]
+		Pop,    // [accumilator + (accumilator + index), index]
+		// stack: [accumilator, index]
+		Incr,             // [accumilator, index]
+		Get(1),           // [accumilator, index, index]
+		Push(100.),       // [accumilator, index, index, 100]
+		Sub,              // [accumilator, index, index - 100]
+		JumpNotEqual0(2), // [accumilator, index] <- Loop end
+		Get(0),           // [accumilator, index, accumilator]
+		Print,
 	]);
 
 	assert_eq!(result, Some(100.));
