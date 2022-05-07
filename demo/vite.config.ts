2ca71ed988cf1580b1bd462fe270c6d832df831a
Roee Shapira <ro33.sha@gmail.com>
2022-05-08 01:07:54 +0300

    Fix demo wasm config

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -6,4 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), checker({ typescript: true }), VitePWA()],
+	server: {
+		fs: { allow: ['.', '../crates/utils/js_bind/pkg'] },
+	},
 });
