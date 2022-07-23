305fe9028d5952834c0641bc55b614bfe4b200de
Roee Shapira <ro33.sha@gmail.com>
2022-07-24 00:26:52 +0300

    Add var call codegen

--- crates/utils/bytecode/src/codegen.rs
+++ crates/utils/bytecode/src/codegen.rs
@@ -1,10 +1,12 @@
-use super::instr::{Instr, Program};
-use anyhow::Result;
-use ast::types::{literal::Literal, node::Node, var_decl::VarDecl};
+use super::instr::{Instr, Pointer, Program};
+use anyhow::{anyhow, Result};
+use ast::types::{literal::Literal, node::Node};
+use std::collections::BTreeMap;
 
 #[derive(Default)]
 pub struct CodeGen {
 	program: Program,
+	variables: BTreeMap<String, Pointer>,
 }
 impl CodeGen {
 	pub fn run(&mut self, tree: &Node) -> Result<Program> {
@@ -14,10 +16,23 @@ impl CodeGen {
 					self.run(instr)?;
 				}
 			}
-			Node::VarDecl(VarDecl { value, .. }) => match &**value {
-				Node::Literal(Literal::Number(n)) => self.program.push(Instr::Push(*n)),
-				other => unimplemented!("{:?} type is not supported yet", other),
+			Node::VarDecl(var_decl) => match &*var_decl.value {
+				Node::Literal(Literal::Number(n)) => {
+					let var_name = var_decl.get_name().join(".");
+
+					self.variables.insert(var_name, self.program.len());
+					self.program.push(Instr::Push(*n));
+				}
+				other => unimplemented!("{:?} type is not supported yet", &other),
 			},
+			Node::VarCall(var_name) => {
+				let var_name = String::from_utf8(var_name.to_vec())?;
+				let pointer = self.variables.get(&var_name).ok_or_else(|| {
+					anyhow!("Could not find the requested variable `{}`", &var_name)
+				})?;
+
+				self.program.push(Instr::Get(*pointer));
+			}
 			other => unimplemented!("{:?}", other),
 		}
 		Ok(self.program.to_owned())
--- crates/utils/bytecode/src/codegen_test.rs
+++ crates/utils/bytecode/src/codegen_test.rs
@@ -30,3 +30,37 @@ fn number_decl() -> Result<()> {
 	assert_eq!(prog, vec![Instr::Push(1.)]);
 	Ok(())
 }
+
+#[test]
+fn number_decl_and_call() -> Result<()> {
+	let tree = Node::Block(vec![
+		Node::VarDecl(VarDecl {
+			var_type: VarType::Const,
+			name: vec![b"param_1"],
+			type_annotation: None,
+			value: Box::new(Node::Literal(Literal::Number(1.))),
+		}),
+		Node::VarCall(b"param_1"),
+	]);
+	let mut codegen = CodeGen::default();
+	let prog = codegen.run(&tree)?;
+
+	assert_eq!(prog, vec![Instr::Push(1.), Instr::Get(0)]);
+	Ok(())
+}
+
+#[test]
+#[should_panic = "Could not find the requested variable `does_not_exist`"]
+fn number_decl_and_call_wrong_param() {
+	let tree = Node::Block(vec![
+		Node::VarDecl(VarDecl {
+			var_type: VarType::Const,
+			name: vec![b"param_1"],
+			type_annotation: None,
+			value: Box::new(Node::Literal(Literal::Number(1.))),
+		}),
+		Node::VarCall(b"does_not_exist"),
+	]);
+	let mut codegen = CodeGen::default();
+	let _prog = codegen.run(&tree).unwrap();
+}
