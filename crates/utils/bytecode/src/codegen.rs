feb6a41f5c520a97fbe8f6bcff2d2963df7aa714
Roee Shapira <ro33.sha@gmail.com>
2022-08-02 00:25:52 +0300

    Add support for builtin functions

--- crates/utils/bytecode/src/codegen.rs
+++ crates/utils/bytecode/src/codegen.rs
@@ -7,8 +7,15 @@ use std::collections::BTreeMap;
 pub struct CodeGen {
 	program: Program,
 	variables: BTreeMap<String, Pointer>,
+	functions: BTreeMap<String, Pointer>,
 }
 impl CodeGen {
+	fn get_var(&self, var_name: &str) -> Result<&Pointer> {
+		self.variables
+			.get(var_name)
+			.ok_or_else(|| anyhow!("Could not find the requested variable `{}`", &var_name))
+	}
+
 	pub fn run(&mut self, tree: &Node) -> Result<Program> {
 		match tree {
 			Node::Block(instr_list) => {
@@ -27,11 +34,26 @@ impl CodeGen {
 			},
 			Node::VarCall(var_name) => {
 				let var_name = String::from_utf8(var_name.to_vec())?;
-				let pointer = self.variables.get(&var_name).ok_or_else(|| {
-					anyhow!("Could not find the requested variable `{}`", &var_name)
-				})?;
+				let pointer = self.get_var(&var_name)?;
 
 				self.program.push(Instr::Get(*pointer));
+			}
+			Node::FnCall(fn_call) => {
+				let fn_name = fn_call.get_name().join(".");
+				let params: Vec<Pointer> = fn_call
+					.params
+					.iter()
+					.map(|param| {
+						let name = param.get_name().join(".");
+						*self.get_var(&name).unwrap()
+					})
+					.collect();
+
+				if self.functions.contains_key(&fn_name) {
+					unimplemented!("Function creation isn't supported yet");
+				} else {
+					self.program.push(Instr::CallBuiltin(fn_name, params));
+				}
 			}
 			other => unimplemented!("{:?}", other),
 		}
