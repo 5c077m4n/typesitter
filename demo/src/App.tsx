bf3a5b326048fa51dd9af4f346992a9d975b355f
Roee Shapira <ro33.sha@gmail.com>
2022-08-03 09:37:02 +0300

    Add eval fn to frontend demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import ReactJson from 'react-json-view';
-import init, { tokenize, build_ast as buildAST } from 'js_bind';
+import init, { tokenize, build_ast as buildAST, ts_eval as tsEval } from 'js_bind';
 
 function Main() {
 	const [script, setScript] = useState('');
@@ -23,21 +23,34 @@ function Main() {
 			return {};
 		}
 	}, [script]);
+	const { result, time: evalTime } = useMemo(() => {
+		try {
+			const start = performance.now();
+			const result = tsEval(script);
+			return { result, time: performance.now() - start };
+		} catch (e) {
+			return {};
+		}
+	}, [script]);
 
 	return (
-		<main className="w-full h-full p-4">
+		<main className="w-full h-full p-4 space-y-4">
 			<textarea
 				className="p-2 w-full border border-black rounded font-mono"
 				value={script}
 				onChange={(e) => setScript(e.target.value)}
 			/>
-			<details open className="mt-4">
+			<details open>
 				<summary>Tokenizer ({tokenTime}ms)</summary>
 				<ReactJson src={tokens} />
 			</details>
-			<details open className="mt-4">
+			<details open>
 				<summary>AST ({astTime}ms)</summary>
 				<ReactJson src={ast} />
+			</details>
+			<details open>
+				<summary>Result ({evalTime}ms)</summary>
+				<textarea className="p-2 w-full border border-black rounded font-mono" value={result} disabled />
 			</details>
 		</main>
 	);
