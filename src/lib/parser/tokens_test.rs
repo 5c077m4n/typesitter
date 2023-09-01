4c9f4abdff7fd373aadae28fe97522e05ff015d4
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-02 00:08:16 +0300

    Add parsers

--- src/lib/parser/tokens_test.rs
+++ src/lib/parser/tokens_test.rs
@@ -14,3 +14,23 @@ fn binary_with_dashes_test() {
 fn binary_test() {
 	assert_eq!(binary("0b0101"), Ok(("", "0101")));
 }
+
+#[test]
+fn string_single_quote_test() {
+	assert_eq!(string(r#"'234'"#), Ok(("", "234")));
+}
+
+#[test]
+fn string_double_quote_test() {
+	assert_eq!(string(r#""2 34""#), Ok(("", "2 34")));
+}
+
+#[test]
+fn const_var_init_test() {
+	assert_eq!(var_init(r#"const a = 1234;"#), Ok(("", ("a", "1234"))));
+}
+
+#[test]
+fn let_var_init_test() {
+	assert_eq!(var_init(r#"let a = 'qwerty';"#), Ok(("", ("a", "'qwerty'"))));
+}
