6179ca64ef72b1b3c9a1204e14c0b50ba088fbaf
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-02 00:08:29 +0300

    Add node to instr codegen

--- crates/utils/bytecode/src/codegen.rs
+++ crates/utils/bytecode/src/codegen.rs
@@ -1,8 +1,26 @@
 use super::instr::{Instr, Program};
 use anyhow::Result;
-use ast::types::node::Node;
+use ast::types::{literal::Literal, node::Node, var_decl::VarDecl};
 
-pub fn codegen(_tree: &Node) -> Result<Program> {
-	let prograpm: Vec<Instr> = Vec::new();
-	Ok(prograpm)
+fn convert<'c>(tree: &'c Node, prog: &'c mut Vec<Instr>) -> Result<&'c Vec<Instr>> {
+	match tree {
+		Node::Block(instr_list) => {
+			for instr in instr_list {
+				convert(instr, prog)?;
+			}
+		}
+		Node::VarDecl(VarDecl { value, .. }) => match &**value {
+			Node::Literal(Literal::Number(n)) => prog.push(Instr::Push(*n)),
+			other => unimplemented!("{:?} type is not supported yet", other),
+		},
+		other => unimplemented!("{:?}", other),
+	}
+	Ok(prog)
+}
+
+pub fn codegen(tree: &Node) -> Result<Program> {
+	let mut program: Vec<Instr> = Vec::new();
+	let program = convert(tree, &mut program)?;
+
+	Ok(program.to_owned())
 }
