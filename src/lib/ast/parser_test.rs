e68f492695ec54b37f2d02947e7a557c6de6ad68
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-02 00:08:20 +0300

    Make parse return type result

--- src/lib/ast/parser_test.rs
+++ src/lib/ast/parser_test.rs
@@ -4,10 +4,21 @@ use crate::lib::{ast::parser::parse, lexer::scanner::scan};
 
 #[test]
 #[ignore]
-pub fn parse_const_number_init_test() -> Result<()> {
+pub fn parse_const_number_init_number_test() -> Result<()> {
 	let init_str = "const n: number = 123;";
 	let tokens = scan(init_str, Some("Parser test".to_string()));
-	let ast = *parse(tokens);
+	let ast = *parse(tokens)?;
+
+	assert_eq!(ast, Vec::new());
+	Ok(())
+}
+
+#[test]
+#[ignore]
+pub fn parse_let_number_init_string_test() -> Result<()> {
+	let init_str = r#"let s: string = '123';"#;
+	let tokens = scan(init_str, Some("Parser test".to_string()));
+	let ast = *parse(tokens)?;
 
 	assert_eq!(ast, Vec::new());
 	Ok(())
