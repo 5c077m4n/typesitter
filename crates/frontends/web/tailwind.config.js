dd8b65a1eb5810fb82282548ba01e362d7e65f9b
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-02 00:08:26 +0300

    Add web frontend

---
+++ crates/frontends/web/tailwind.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	content: ['./index.html', './src/**/*.{ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
---
+++ crates/frontends/web/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
+import wasmPack from 'vite-plugin-wasm-pack';
+import { VitePWA } from 'vite-plugin-pwa';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), checker({ typescript: true }), wasmPack('../../utils/js_bind'), VitePWA()],
+});
