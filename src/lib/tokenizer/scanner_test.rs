9e97cd2071670674a706acf4d74faf0b01dc3163
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-02 00:08:19 +0300

    Add scanner test

--- src/lib/tokenizer/scanner_test.rs
+++ src/lib/tokenizer/scanner_test.rs
@@ -27,6 +27,31 @@ fn scan_basic_number_init_test() -> anyhow::Result<()> {
 }
 
 #[test]
+fn scan_typed_number_init_test() -> anyhow::Result<()> {
+	let input = r#"const a: number = 123;"#;
+	let token_values: Vec<TokenType> = scan(input, Some("test".to_owned()))
+		.filter_map(|t| match t.value {
+			TokenType::Generic(frag) if frag.trim() == "" => None,
+			other => Some(other),
+		})
+		.collect();
+
+	assert_eq!(
+		token_values,
+		vec![
+			TokenType::Keyword(Keyword::Const),
+			TokenType::Identifier("a"),
+			TokenType::Punctuation(Punctuation::Colon),
+			TokenType::Identifier("number"),
+			TokenType::Punctuation(Punctuation::Equal),
+			TokenType::Literal(Literal::Number(123.)),
+			TokenType::Punctuation(Punctuation::Semicolon)
+		]
+	);
+	Ok(())
+}
+
+#[test]
 fn scan_basic_string_init_test() -> anyhow::Result<()> {
 	let input = r#"const a = 'what?!';"#;
 	let token_values: Vec<TokenType> = scan(input, Some("test".to_owned()))
