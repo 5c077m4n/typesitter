e8c4056692fc1a0c673dd91f8a56b762b652ad4a
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-02 00:08:25 +0300

    Add compile fn

--- crates/backends/jit/src/compiler.rs
+++ crates/backends/jit/src/compiler.rs
@@ -10,7 +10,8 @@ use inkwell::{
 	builder::Builder,
 	context::Context,
 	module::Module,
-	types::AnyTypeEnum,
+	passes::PassManager,
+	types::{AnyTypeEnum, BasicMetadataTypeEnum},
 	values::{BasicValueEnum, FunctionValue},
 };
 
@@ -18,17 +19,20 @@ pub struct Compiler<'c, 'ctx> {
 	context: &'ctx Context,
 	module: &'c Module<'ctx>,
 	builder: &'c Builder<'ctx>,
+	pass_manager: &'c PassManager<FunctionValue<'ctx>>,
 }
 impl<'c, 'ctx> Compiler<'c, 'ctx> {
-	fn compile_fn(&self, fn_decl: &FnDecl) -> Result<FunctionValue<'ctx>> {
+	fn compile_fn_signature(&self, fn_decl: &FnDecl) -> Result<FunctionValue<'ctx>> {
 		let fn_return_type = match &fn_decl.return_type {
 			Some(TypeAnnotation::Number) => AnyTypeEnum::FloatType(self.context.f64_type()),
 			None | Some(TypeAnnotation::Void) => AnyTypeEnum::VoidType(self.context.void_type()),
 			other => todo!("Support other function types ({:?})", other),
 		};
+		// TODO: get real input types from `fn_decl.inputs`
+		let fn_input_type: Vec<BasicMetadataTypeEnum> = Vec::new();
 		let fn_type = match fn_return_type {
-			AnyTypeEnum::FloatType(ret_type) => ret_type.fn_type(&[], false),
-			AnyTypeEnum::VoidType(ret_type) => ret_type.fn_type(&[], false),
+			AnyTypeEnum::FloatType(ret_type) => ret_type.fn_type(&fn_input_type, false),
+			AnyTypeEnum::VoidType(ret_type) => ret_type.fn_type(&fn_input_type, false),
 			other => todo!("Support {:?} type", &other),
 		};
 
@@ -41,13 +45,6 @@ impl<'c, 'ctx> Compiler<'c, 'ctx> {
 			fn_type,
 			None,
 		);
-		let basic_block = self.context.append_basic_block(func_val, "entry");
-		self.builder.position_at_end(basic_block);
-
-		todo!(
-			"continue the `compile_instr` function and then continue this function (`compile_fn`)"
-		);
-
 		Ok(func_val)
 	}
 
@@ -90,14 +87,26 @@ impl<'c, 'ctx> Compiler<'c, 'ctx> {
 				self.compile_var_decl(var_decl)?;
 			}
 			Node::FnDecl(fn_decl) => {
-				self.compile_fn(fn_decl)?;
+				self.compile_fn_signature(fn_decl)?;
 			}
 			other => unimplemented!("Compile instruction for {:?}", other),
 		}
 		Ok(())
 	}
 
-	pub fn compile(&self, tree: &Node) -> Result<()> {
-		self.compile_instr(tree)
+	pub fn compile(
+		context: &'ctx Context,
+		module: &'c Module<'ctx>,
+		builder: &'c Builder<'ctx>,
+		pass_manager: &'c PassManager<FunctionValue<'ctx>>,
+		tree: &Node,
+	) -> Result<()> {
+		let compiler = Compiler {
+			context,
+			module,
+			builder,
+			pass_manager,
+		};
+		compiler.compile_instr(tree)
 	}
 }
