4ee641149003d8d4c6f559a4b31cfe17c488a8c0
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-02 00:08:27 +0300

    Replace netlify with gh pages

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
-import wasmPack from 'vite-plugin-wasm-pack';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), checker({ typescript: true }), wasmPack('../crates/utils/js_bind'), VitePWA()],
+	plugins: [react(), checker({ typescript: true }), VitePWA()],
 });
