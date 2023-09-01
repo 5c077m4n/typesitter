6e42e156f80a645dba4384c8c44f4519e68085ac
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2023-09-02 00:08:21 +0300

    Fix parse rec call

--- src/lib/ast/parser/parse_test.rs
+++ src/lib/ast/parser/parse_test.rs
@@ -20,7 +20,7 @@ pub fn parse_const_init_number_test() -> Result<()> {
 
 	assert_eq!(
 		ast,
-		vec![Box::new(Node::VarDec(VarDec {
+		vec![Box::new(Node::VarDecl(VarDec {
 			var_type: VarType::Const,
 			name: "n",
 			type_annotation: Some("number"),
@@ -41,13 +41,13 @@ pub fn parse_2_const_init_number_test() -> Result<()> {
 	assert_eq!(
 		ast,
 		vec![
-			Box::new(Node::VarDec(VarDec {
+			Box::new(Node::VarDecl(VarDec {
 				var_type: VarType::Const,
 				name: "n",
 				type_annotation: Some("number"),
 				value: Box::new(Node::Literal(Literal::Number(123.)))
 			})),
-			Box::new(Node::VarDec(VarDec {
+			Box::new(Node::VarDecl(VarDec {
 				var_type: VarType::Const,
 				name: "n",
 				type_annotation: Some("number"),
@@ -66,7 +66,7 @@ pub fn parse_let_init_string_test() -> Result<()> {
 
 	assert_eq!(
 		ast,
-		vec![Box::new(Node::VarDec(VarDec {
+		vec![Box::new(Node::VarDecl(VarDec {
 			var_type: VarType::Let,
 			name: "s",
 			type_annotation: Some("string"),
@@ -84,7 +84,7 @@ pub fn parse_let_number_init_no_type_test() -> Result<()> {
 
 	assert_eq!(
 		ast,
-		vec![Box::new(Node::VarDec(VarDec {
+		vec![Box::new(Node::VarDecl(VarDec {
 			var_type: VarType::Let,
 			name: "x",
 			type_annotation: None,
@@ -103,7 +103,7 @@ pub fn parse_let_no_int_value_test() -> Result<()> {
 
 	assert_eq!(
 		ast,
-		vec![Box::new(Node::VarDec(VarDec {
+		vec![Box::new(Node::VarDecl(VarDec {
 			var_type: VarType::Let,
 			name: "x",
 			type_annotation: None,
@@ -164,4 +164,34 @@ pub fn bad_parse_2_param_fn_call_test() {
 	let init_str = r#"fnName(, param_1, param_2);"#;
 	let mut tokens = scan(init_str, Some("Parser test".to_string()));
 	let _ast = parse(&mut tokens, None).unwrap();
+}
+
+#[test]
+#[ignore]
+pub fn parse_fn_declaration_test() -> Result<()> {
+	let init_str = r#"function fn1() { conosle.log(123); }"#;
+	let mut tokens = scan(init_str, Some("Parser test".to_string()));
+	let ast = parse(&mut tokens, None)?;
+
+	assert_eq!(
+		ast,
+		vec![Box::new(Node::FnCall(FnCall {
+			fn_name: "fnName",
+			params: vec![
+				VarDec {
+					var_type: VarType::Let,
+					name: "param_1",
+					type_annotation: None,
+					value: Box::new(Node::Literal(Literal::Undefined))
+				},
+				VarDec {
+					name: "param_2",
+					var_type: VarType::Let,
+					type_annotation: None,
+					value: Box::new(Node::Literal(Literal::Undefined)),
+				}
+			],
+		}))]
+	);
+	Ok(())
 }
